e1d060140040efbc78c27b3136074d0df87991f7
optyfr <17027109+optyfr@users.noreply.github.com>
2018-09-26 10:53:06 +0200

    skeleton

---
+++ src/jrm/webui/client/Client.java
@@ -0,0 +1,19 @@
+package jrm.webui.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Client implements EntryPoint
+{
+
+	public Client()
+	{
+		super();
+	}
+
+	@Override
+	public void onModuleLoad()
+	{
+		System.out.println("coucou");
+	}
+
+}
