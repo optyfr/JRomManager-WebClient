7c623c0eb1492cc9758a03b5e056817c33818be9
optyfr <17027109+optyfr@users.noreply.github.com>
2018-09-30 23:31:13 +0200

    Work on web client

--- src/jrm/webui/client/Client.java
+++ src/jrm/webui/client/Client.java
@@ -1,6 +1,13 @@
 package jrm.webui.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Timer;
+import com.sksamuel.gwt.websockets.Websocket;
+import com.sksamuel.gwt.websockets.WebsocketListener;
+import com.smartgwt.client.rpc.RPCCallback;
+import com.smartgwt.client.rpc.RPCManager;
+import com.smartgwt.client.rpc.RPCRequest;
+import com.smartgwt.client.rpc.RPCResponse;
 import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.util.Page;
 import com.smartgwt.client.util.SC;
@@ -8,6 +15,7 @@ import com.smartgwt.client.widgets.Label;
 
 public class Client implements EntryPoint
 {
+	private static String session;
 
 	public Client()
 	{
@@ -17,15 +25,59 @@ public class Client implements EntryPoint
 	@Override
 	public void onModuleLoad()
 	{
-		SC.logInfo("Hello");
-		new Label() {{
-			setWidth100();
-	//		setAutoDraw(true);
-			setAlign(Alignment.CENTER);
-			setContents("<span style='font:bold 24px arial'>JRomManager</span>");
-		}}.draw();
+		new Label()
+		{
+			{
+				setWidth100();
+				setAlign(Alignment.CENTER);
+				setContents("<span style='font:bold 24px arial'>JRomManager</span>");
+			}
+		}.draw();
 		Page.setTitle("JRomManager");
-		new MainWindow();
+		RPCManager.sendRequest(new RPCRequest()
+		{
+			{
+				setActionURL("/session");
+			}
+		}, new RPCCallback()
+		{
+			@Override
+			public void execute(RPCResponse response, Object rawData, RPCRequest request)
+			{
+				if (response.getHttpResponseCode() == 200)
+				{
+					session = rawData.toString();
+					SC.logWarn("rawData: " + rawData.toString());
+					Websocket socket = new Websocket("ws://localhost:8080");
+					socket.addListener(new WebsocketListener() {
+					    @Override
+					    public void onMessage(String msg) {
+					    	SC.logWarn(msg);
+					    }
+
+					    @Override
+					    public void onOpen() {
+					       socket.send("coucou");
+					    }
+
+						@Override
+						public void onClose()
+						{
+						}
+					});
+					socket.open();
+					new MainWindow();
+					new Timer()
+					{
+						@Override
+						public void run()
+						{
+							socket.send(session);
+						}
+					}.scheduleRepeating(10000);
+				}
+			}
+		});
 	}
 
 }
--- src/jrm/webui/client/MainWindow.java
+++ src/jrm/webui/client/MainWindow.java
@@ -3,6 +3,7 @@ package jrm.webui.client;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.smartgwt.client.data.Criteria;
 import com.smartgwt.client.data.OperationBinding;
 import com.smartgwt.client.data.RestDataSource;
 import com.smartgwt.client.data.fields.DataSourceIntegerField;
@@ -11,12 +12,19 @@ import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.types.DSDataFormat;
 import com.smartgwt.client.types.DSOperationType;
 import com.smartgwt.client.types.DSProtocol;
+import com.smartgwt.client.types.PreserveOpenState;
 import com.smartgwt.client.types.TabBarControls;
+import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.IButton;
 import com.smartgwt.client.widgets.Window;
 import com.smartgwt.client.widgets.events.CloseClickEvent;
 import com.smartgwt.client.widgets.events.CloseClickHandler;
+import com.smartgwt.client.widgets.grid.HoverCustomizer;
 import com.smartgwt.client.widgets.grid.ListGrid;
+import com.smartgwt.client.widgets.grid.ListGridField;
+import com.smartgwt.client.widgets.grid.ListGridRecord;
+import com.smartgwt.client.widgets.grid.events.RecordClickEvent;
+import com.smartgwt.client.widgets.grid.events.RecordClickHandler;
 import com.smartgwt.client.widgets.layout.SplitPane;
 import com.smartgwt.client.widgets.layout.VLayout;
 import com.smartgwt.client.widgets.tab.Tab;
@@ -30,8 +38,8 @@ public class MainWindow extends Window
 	{
 		super();
 		setTitle("JRomManager");
-		setWidth(640);
-		setHeight(400);
+		setWidth(800);
+		setHeight(600);
 		setAnimateMinimize(true);
 		setCanDragReposition(true);
 		setCanDragResize(true);
@@ -53,6 +61,7 @@ public class MainWindow extends Window
 			}
 		});
 		addItem(new TabSet() {{
+			setPaneMargin(0);
 			setTabBarControls(
 				TabBarControls.TAB_SCROLLER,
 				TabBarControls.TAB_PICKER
@@ -62,32 +71,88 @@ public class MainWindow extends Window
 				setPane(new VLayout() {{
 					addMembers(
 						new SplitPane() {{
+							ListGrid listgrid = new ListGrid() {{
+								setShowFilterEditor(false);
+								setShowHover(true);
+								setCanHover(true);
+								setHoverWidth(200);
+								setDataSource(new RestDataSource() {{
+									setID("profilesList");
+									setDataFormat(DSDataFormat.XML);
+									setDataURL("/datasources/"+getID());
+									setOperationBindings(
+										new OperationBinding(){{setOperationType(DSOperationType.FETCH);setDataProtocol(DSProtocol.POSTXML);}}
+									);
+							        DataSourceTextField nameField = new DataSourceTextField("Name");
+							        DataSourceTextField pathField = new DataSourceTextField("Path");
+							        pathField.setHidden(true);
+							        DataSourceTextField verField = new DataSourceTextField("version");
+							        DataSourceTextField haveSetsField = new DataSourceTextField("haveSets");
+							        DataSourceTextField haveRomsField = new DataSourceTextField("haveRoms");
+							        DataSourceTextField haveDisksField = new DataSourceTextField("haveDisks");
+							        DataSourceTextField createdField = new DataSourceTextField("created");
+							        DataSourceTextField scannedField = new DataSourceTextField("scanned");
+							        DataSourceTextField fixedField = new DataSourceTextField("fixed");
+							        setFields(nameField, pathField, verField, haveSetsField, haveRomsField, haveDisksField, createdField, scannedField, fixedField);
+								}});
+							}};
 							setNavigationPane(new TreeGrid() {{
 								setShowRoot(true);
 								setAutoFetchData(true);
+								setLoadDataOnDemand(false);
+								setAutoFitFieldWidths(true);
+								setAutoPreserveOpenState(PreserveOpenState.ALWAYS);
+								setIndentSize(10);
+								setExtraIconGap(0);
+								setShowHover(true);
+								setHoverWidth(200);
+								setCanHover(true);
+								addRecordClickHandler(new RecordClickHandler()
+								{
+									@Override
+									public void onRecordClick(RecordClickEvent event)
+									{
+										String path = event.getRecord().getAttribute("Path");
+										listgrid.invalidateCache();
+										listgrid.fetchData(new Criteria() {{addCriteria("Path", path==null?"":path);}});
+									}
+								});
+								//setShowConnectors(true);
 								setDataSource(new RestDataSource() {{
 									setID("profilesTree");
-							        setTitleField("Name");  
-							        setRecordXPath("/List/Dir");
-							        DataSourceTextField nameField = new DataSourceTextField("Name", "Name", 128);  
-							        DataSourceIntegerField IDField = new DataSourceIntegerField("ID", "ID");  
-							        IDField.setPrimaryKey(true);  
-							        IDField.setRequired(true);  
-							        DataSourceIntegerField parentIDField = new DataSourceIntegerField("parentId", "Parent");  
-							        parentIDField.setRequired(true);  
-							        parentIDField.setForeignKey(id + ".ID");  
-							        parentIDField.setRootValue("0");
-							        setFields(nameField, IDField, parentIDField);  
+							        //setRequestProperties(new DSRequest() {{setData(Collections.singletonMap("pipo", "papa"));}});
 									setOperationBindings(
-										new OperationBinding(){{setOperationType(DSOperationType.FETCH);setDataProtocol(DSProtocol.POSTPARAMS);}}
+										new OperationBinding(){{setOperationType(DSOperationType.FETCH);setDataProtocol(DSProtocol.POSTXML);}}
 									);
 									setDataFormat(DSDataFormat.XML);
 									setDataURL("/datasources/"+getID());
+							        DataSourceTextField nameField = new DataSourceTextField("title");
+							        DataSourceTextField pathField = new DataSourceTextField("Path");
+							        pathField.setHidden(true);
+							        DataSourceIntegerField IDField = new DataSourceIntegerField("ID");  
+							        IDField.setPrimaryKey(true);  
+							        IDField.setRequired(true);  
+							        DataSourceIntegerField parentIDField = new DataSourceIntegerField("ParentID");  
+							        parentIDField.setRequired(true);  
+							        parentIDField.setForeignKey(id + ".ID");  
+							        parentIDField.setRootValue("0");
+							        setFields(nameField, pathField, IDField, parentIDField);  
+								}});
+								setFields(new ListGridField("title") {{
+									setHoverCustomizer(new HoverCustomizer()
+									{
+										
+										@Override
+										public String hoverHTML(Object value, ListGridRecord record, int rowNum, int colNum)
+										{
+											SC.logWarn(record.getAttribute("title"));
+											return record.getAttribute("title");
+										}
+									});
 								}});
 							}});
-							setListPane(new ListGrid() {{
-								setShowFilterEditor(false);
-							}});
+							setNavigationPaneWidth(200);
+							setListPane(listgrid);
 						}},
 						new ToolStrip() {{
 							setAlign(Alignment.CENTER);
@@ -113,7 +178,7 @@ public class MainWindow extends Window
 				setTitle("Settings");
 			}});
 		}});
-		//centerInPage();
+		centerInPage();
 		show();
 	}
 
