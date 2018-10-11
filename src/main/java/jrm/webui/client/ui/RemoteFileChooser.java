c3cf695d41a40a2f07acebc01e7f04d2643a0dd8
optyfr <17027109+optyfr@users.noreply.github.com>
2018-10-11 23:46:01 +0200

    RemoteFileChooser

--- src/main/java/jrm/webui/client/ui/RemoteFileChooser.java
+++ src/main/java/jrm/webui/client/ui/RemoteFileChooser.java
@@ -4,14 +4,19 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.smartgwt.client.data.*;
+import com.smartgwt.client.data.DSRequest;
+import com.smartgwt.client.data.DSResponse;
+import com.smartgwt.client.data.OperationBinding;
+import com.smartgwt.client.data.Record;
+import com.smartgwt.client.data.RestDataSource;
+import com.smartgwt.client.data.ResultSet;
+import com.smartgwt.client.data.XMLTools;
 import com.smartgwt.client.data.fields.DataSourceBooleanField;
 import com.smartgwt.client.data.fields.DataSourceTextField;
 import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.types.DSDataFormat;
 import com.smartgwt.client.types.DSOperationType;
 import com.smartgwt.client.types.DSProtocol;
-import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.IButton;
 import com.smartgwt.client.widgets.Label;
 import com.smartgwt.client.widgets.Window;
@@ -19,6 +24,7 @@ import com.smartgwt.client.widgets.grid.ListGrid;
 import com.smartgwt.client.widgets.grid.ListGridField;
 import com.smartgwt.client.widgets.grid.ListGridRecord;
 import com.smartgwt.client.widgets.layout.HLayout;
+import com.smartgwt.client.widgets.layout.LayoutSpacer;
 import com.smartgwt.client.widgets.layout.SplitPane;
 
 import jrm.webui.client.Client;
@@ -38,6 +44,14 @@ public final class RemoteFileChooser extends Window
 		super();
 		setWidth(500);
 		setHeight(400);
+		switch(context)
+		{
+			case "tfRomsDest":
+				setTitle("Choose a directory");
+			default:
+				setTitle("Choose a file or directory");
+				break;
+		}
 		setAutoCenter(true);
 		setIsModal(true);
 		setCanDragResize(true);
@@ -46,7 +60,34 @@ public final class RemoteFileChooser extends Window
 		addItem(new SplitPane() {{
 			setHeight("*");
 			setNavigationPane(new ListGrid() {{
-				//TODO implements navigation grid
+				setShowFilterEditor(false);
+				setShowHover(true);
+				setCanHover(true);
+				setHoverWidth(200);
+				setAutoFetchData(true);
+				addRecordClickHandler(event->{
+					list.getDataSource().setRequestProperties(new DSRequest() {{
+						Map<String,String> params = new HashMap<>();
+						params.put("context", context);
+						params.put("parent", event.getRecord().getAttribute("Path"));
+						setData(params);
+					}});
+					list.invalidateCache();
+				});
+				setDataSource(new RestDataSource() {{
+					setID("remoteRootChooser");
+					setDataURL("/datasources/"+getID());
+					setRequestProperties(new DSRequest() {{setData(Collections.singletonMap("context", context));}});
+					setDataFormat(DSDataFormat.XML);
+					setOperationBindings(
+						new OperationBinding(){{setOperationType(DSOperationType.FETCH);setDataProtocol(DSProtocol.POSTXML);}}
+					);
+					DataSourceTextField nameField = new DataSourceTextField("Name", Client.session.getMsg("FileTableModel.Profile"));
+					DataSourceTextField pathField = new DataSourceTextField("Path");
+					pathField.setHidden(true);
+					pathField.setPrimaryKey(true);
+					setFields(nameField, pathField);
+				}});
 			}});
 			setDetailPane(list=new ListGrid() {{
 				setShowFilterEditor(false);
@@ -76,8 +117,6 @@ public final class RemoteFileChooser extends Window
 						RemoteFileChooser.this.markForDestroy();
 					}
 				});
-				addDataArrivedHandler(event->{
-				});
 				setDataSource(new RestDataSource() {
 					{
 						setID("remoteFileChooser");
@@ -96,8 +135,7 @@ public final class RemoteFileChooser extends Window
 					}
 					@Override
 					protected void transformResponse(DSResponse dsResponse, DSRequest dsRequest, Object data) {
-						SC.logWarn("parent="+dsResponse.getAttribute("parent"));
-						parent.setContents(dsResponse.getAttribute("parent"));
+						parent.setContents(XMLTools.selectString(data, "/response/parent"));
 						super.transformResponse(dsResponse, dsRequest, data);
 					};
 				});
@@ -113,20 +151,22 @@ public final class RemoteFileChooser extends Window
 			}});
 			setDetailToolButtons(parent=new Label("parent") {{
 				setWidth100();
-				setBorder("1xp inset");
-				setShadowDepth(1);
-				setShadowOffset(2);
+				setBorder("1px inset");
 			}});
 			setNavigationPaneWidth(100);
 		}});
 		addItem(new HLayout() {{
 			setHeight(20);
 			setLayoutAlign(Alignment.RIGHT);
+			setPaddingAsLayoutMargin(true);
+			setPadding(5);
+			setMembersMargin(5);
 			setMembers(
 				new IButton("Cancel") {{
 					setAutoFit(true);
 					addClickHandler(e->RemoteFileChooser.this.markForDestroy());
 				}},
+				new LayoutSpacer() {{setWidth("*");}},
 				new IButton("Choose") {{
 					setAutoFit(true);
 					addClickHandler(e->{
