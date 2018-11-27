e4c36b95de4b68504b4dc6a54cad4603c12198dc
optyfr <17027109+optyfr@users.noreply.github.com>
2018-11-27 17:07:09 +0100

    Batch Compressor skeleton (web)

---
+++ src/main/java/jrm/webui/client/ui/BatchCompressorPanel.java
@@ -0,0 +1,12 @@
+package jrm.webui.client.ui;
+
+import com.smartgwt.client.widgets.layout.VLayout;
+
+public class BatchCompressorPanel extends VLayout
+{
+
+	public BatchCompressorPanel()
+	{
+		// TODO Auto-generated constructor stub
+	}
+}
